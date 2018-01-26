7f1ebea9fe525dfde50bec03628a55f597b279b3
cavoixanhttnb <35825622+cavoixanhttnb@users.noreply.github.com>
2018-01-26 16:58:39 +0700

    re-add project

---
+++ POC/POC/POC/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace POC
+{
+	public partial class MainPage : ContentPage
+	{
+        Label lblStatus;
+        Editor txtDescription;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Label header = new Label
+            {
+                Text = "Status: ",
+                HorizontalOptions = LayoutOptions.Start
+            };
+            lblStatus = new Label
+            {
+                Text = "Not Connect",
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Button button = new Button
+            {
+                Text = "Connect",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            button.Clicked += OnConnectClicked;
+
+            txtDescription = new Editor
+            {
+                Text = "Multi text",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    lblStatus,
+                    button,
+                    txtDescription
+                }
+            };
+        }
+
+        void OnConnectClicked(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Connected";
+            txtDescription.Text = "Click 1";
+        }
+
+    }
+}
